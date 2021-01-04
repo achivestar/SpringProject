de8a9dc0731d631a3cc76e443ec99113cc6ac22e
achivestar <achivestar@gmain.com>
2021-01-04 22:42:34 +0900

    Ajax를 이용한 아이디 중복확인 기능 추가

---
+++ MiniProjectJava/src/kr/co/softcampus/service/UserService.java
@@ -0,0 +1,25 @@
+package kr.co.softcampus.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.softcampus.dao.UserDAO;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserDAO userDao;
+	
+	public boolean checkUserIdExist(String user_id) {
+		String user_name = userDao.checkUserIdExist(user_id);
+		
+		
+		if(user_name == null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+}
